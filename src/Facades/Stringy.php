34b8f43ef34308bb47af013a8e9ac35cd8d7b9b3
Brian Faust <hello@brianfaust.de>
2016-12-04 13:03:36 +0200

    More php 7.1 return types

--- src/Facades/Stringy.php
+++ src/Facades/Stringy.php
@@ -26,7 +26,7 @@ use Illuminate\Support\Facades\Facade;
 
 class Stringy extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'stringy';
     }
